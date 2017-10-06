2bd8a8c24c8135ca6839fc29354398c45e9d72e1
l4u532 <k.langenheldt@gmail.com>
2017-10-06 15:44:34 +0200

    add facebook prophet add dataset for polygon 13

--- ts-fbprophet.py
+++ ts-fbprophet.py
@@ -6,17 +6,18 @@ from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 matplotlib.rcParams['axes.color_cycle'] = ['orange', 'lightblue', 'grey']
 
-hours_to_predict = 10
+# setup forecast and cross-validation
+fc_hours_to_predict = 10
 cv_horizon = '4 hour'   # options: hour, day, week, month
+
+# setup data
+nrows = None
+datasets = ['raw15_SLICE15M_13.csv', 'raw15_SLICE15M_10.csv', 'raw15_SLICE15M_3.csv']
 basepath = 'data/'
-# file = 'raw15_SLICE15M_13.csv'
-# data = basepath + file
-
-arr_data = ['raw15_SLICE15M_13.csv', 'raw15_SLICE15M_10.csv', 'raw15_SLICE15M_3.csv']
 results = []
-nrows = 100
 
-def predict(data):
+# create forecast model and plot, save plot under /img
+def forecast(data):
     # dataset
     df = pd.read_csv(data, nrows=nrows)
     # prep for Prophet
@@ -25,7 +26,7 @@ def predict(data):
     # predict
     model.fit(df)
     print('model fitted.')
-    future = model.make_future_dataframe(periods=hours_to_predict * 4, freq='H')
+    future = model.make_future_dataframe(periods=fc_hours_to_predict * 4, freq='H')
     future.tail()
     print('df created.')
     forecast = model.predict(future)
@@ -36,12 +37,12 @@ def predict(data):
     print('plot created.')
     return model
 
-
+# calculate MAPE (timeseries evaluation metric)
 def mean_absolute_percentage_error(y_true, y_pred):
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
-
+# cross-validate prediction for model evaluation
 def crossvalidate(model, filename):
     from fbprophet.diagnostics import cross_validation
     df_cv = cross_validation(model, horizon=cv_horizon)  # hour, day, week (singular)
@@ -56,10 +57,8 @@ def crossvalidate(model, filename):
     df_cv.plot()
     savefig('img/' + 'cv_' + filename + '.png', bbox_inches='tight')
 
-for file in arr_data:
+for file in datasets:
     data = basepath + file
-    model = predict(data)
+    model = forecast(data)
     crossvalidate(model, file)
     print(results)
-
-#show()  # show all plots
