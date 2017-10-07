3b96cc9c1d4c792854e1fc4e831fcd1968381b4b
l4u532 <k.langenheldt@gmail.com>
2017-10-07 17:35:23 +0200

    lol

--- ts-fbprophet.py
+++ ts-fbprophet.py
@@ -1,37 +1,41 @@
-import matplotlib
+import datetime
+
+import dateutil
 from fbprophet import Prophet
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
-matplotlib.rcParams['axes.color_cycle'] = ['orange', 'lightblue', 'grey']
+plt.rcParams['axes.color_cycle'] = ['orange', 'lightblue', 'grey']
 
 # setup forecast
 fc_hours_to_predict = 2 # how far forecast looks into future
 # setup cv
-cv_horizon_amount = 4       # how far cv looks into future
+cv_horizon_amount = 24       # how far cv looks into future
 cv_horizon_unit = 'hour'    # units: sec, minute, hour, day, week, month
+hr_range_arr = [24, 12, 8, 4, 2, 1] # state model accuracy for these hours for RMSE, R^2, MAPE
 
 # setup data
 slices_per_hour = 4         # 15m = 4, 30m = 2, 60m = 1
+startdate = dateutil.parser.parse('2015-06-29 00:00:00')
+enddate = dateutil.parser.parse('2015-07-05 00:00:00')
 nrows = 100
 skiprows = range(1, 150)
-datasets = ['raw15_SLICE15M_13.csv']
-#datasets = ['raw15_SLICE15M_13.csv', 'raw15_SLICE15M_10.csv', 'raw15_SLICE15M_3.csv']
+datasets = ['raw15_SLICE15M_13.csv', 'raw15_SLICE15M_10.csv', 'raw15_SLICE15M_3.csv']
 basepath = 'data/'
 results = []
 
-if nrows < cv_horizon_amount*slices_per_hour*3:
-    raise ValueError("nrows must be > cv_horizon_amount*slices_per_hour*3")
 
 # create forecast model and plot, save plot under /img
 def forecast(data, filename):
     # dataset
-    df = pd.read_csv(data, nrows=nrows, skiprows=skiprows)
-    # prep for Prophet
+    df = pd.read_csv(data, nrows=nrows, skiprows=skiprows, parse_dates=['TIMESTAMP'])
+    # prep cols for Prophet
     df.columns = ['ds', 'y']
-    model = Prophet()
+    # set date range
+    df = df[(df.ds >= startdate & df.ds <= enddate)]
     # predict
+    model = Prophet()
     model.fit(df)
     print('model fitted.')
     future = model.make_future_dataframe(periods=fc_hours_to_predict * 4, freq='H')
@@ -61,7 +65,7 @@ def crossvalidate(model, filename):
 
 def evaluate_cv(df_cv, filename):
     # report scores
-    for hr_range in [24, 12, 8, 4, 2, 1]:
+    for hr_range in hr_range_arr:
         # cut data
         df_cv = df_cv.iloc[0:hr_range*slices_per_hour]
         df_cv = df_cv[['ds', 'y', 'yhat']]
@@ -76,10 +80,10 @@ def evaluate_cv(df_cv, filename):
         print(score)
 
         # plot
-        df_cv = df_cv.set_index('ds')   # set timestamp as index
-        df_cv = df_cv.cumsum()
+        df_plot = df_cv.set_index('ds')   # set timestamp as index
+        df_plot = df_plot.cumsum()
         plt.figure
-        df_cv.plot()
+        df_plot.plot()
         plt.xlabel('timestamp')
         plt.ylabel('attendees')
         plt.title('hours considered: ' + str(hr_range))
