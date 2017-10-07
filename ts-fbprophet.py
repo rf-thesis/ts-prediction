6796c84e7ca026115ac1afa5d5c13f4ffc1600cf
l4u532 <k.langenheldt@gmail.com>
2017-10-07 14:42:55 +0200

    split up cv terminology

--- ts-fbprophet.py
+++ ts-fbprophet.py
@@ -6,16 +6,22 @@ from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 matplotlib.rcParams['axes.color_cycle'] = ['orange', 'lightblue', 'grey']
 
-# setup forecast and cross-validation
+# setup forecast
 fc_hours_to_predict = 2 # how far forecast looks into future
-cv_horizon = '4 hour'   # how far cv looks into future -- options: hour, day, week, month
+# setup cv
+cv_horizon_amount = 4       # how far cv looks into future
+cv_horizon_unit = 'hour'    # units: sec, minute, hour, day, week, month
 
 # setup data
+slices_per_hour = 4         # 15m = 4, 30m = 2, 60m = 1
 nrows = 350
 skiprows = range(1, 150)
 datasets = ['raw15_SLICE15M_13.csv', 'raw15_SLICE15M_10.csv', 'raw15_SLICE15M_3.csv']
 basepath = 'data/'
 results = []
+
+if nrows < cv_horizon_amount*slices_per_hour*3:
+    raise ValueError("nrows must be > cv_horizon_amount*slices_per_hour*3")
 
 # create forecast model and plot, save plot under /img
 def forecast(data, filename):
@@ -48,6 +54,7 @@ def mean_absolute_percentage_error(y_true, y_pred):
 # cross-validate prediction for model evaluation
 def crossvalidate(model, filename):
     from fbprophet.diagnostics import cross_validation
+    cv_horizon = str(cv_horizon_amount) + ' ' + cv_horizon_unit
     df_cv = cross_validation(model, horizon=cv_horizon)  # hour, day, week (singular)
     evaluate_cv(df_cv, filename)
 
@@ -55,7 +62,7 @@ def evaluate_cv(df_cv, filename):
     # report scores
     for hr_range in [24, 12, 8, 4, 2, 1]:
         # cut data
-        df_cv = df_cv.iloc[0:hr_range*4]
+        df_cv = df_cv.iloc[0:hr_range*slices_per_hour]
         df_cv = df_cv[['ds', 'y', 'yhat']]
 
         # define metrics & create score
