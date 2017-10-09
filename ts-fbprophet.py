eb151a008deb3f64654160f798af97e57cbe55ae
l4u532 <k.langenheldt@gmail.com>
2017-10-09 22:25:34 +0200

    add plot for uncertainty

--- ts-fbprophet.py
+++ ts-fbprophet.py
@@ -11,15 +11,15 @@ plt.rcParams['axes.color_cycle'] = ['orange', 'lightblue', 'grey']
 # setup forecast
 fc_hours_to_predict = 2 # how far forecast looks into future
 # setup cv
-cv_horizon_amount = 10       # how far cv looks into future
+cv_horizon_amount = 12       # how far cv looks into future
 cv_horizon_unit = 'hour'    # units: sec, minute, hour, day, week, month
 hr_range_arr = [24, 12, 8, 4, 2, 1] # state model accuracy for these hours for RMSE, R^2, MAPE
 
 # setup data
-plotforecasts, plotcrossvals = False, False
+plotforecasts, plotcrossvals, plotcvuncertainty = False, False, True
 slices_per_hour = 4                                         # 15m = 4, 30m = 2, 60m = 1
-startdate = dateutil.parser.parse('2017-06-23 12:00:00')    # goes from 26-06 to 05-07
-enddate =   dateutil.parser.parse('2017-06-26 12:00:00')
+startdate = dateutil.parser.parse('2017-06-26 12:00:00')    # goes from 26-06 to 05-07
+enddate =   dateutil.parser.parse('2017-06-30 12:00:00')
 filename_data = '2017_devicecount15m.csv'
 filename_pols = '2017_devicecount15m-polygons.csv'
 basepath = 'data/'
@@ -43,6 +43,7 @@ def forecast(df_orig, polygon):
     print('df created.')
     forecast = model.predict(future)
     print('forecast made.')
+
     # plot
     if plotforecasts:
         model.plot(forecast)
@@ -50,6 +51,7 @@ def forecast(df_orig, polygon):
         plt.close()
         plt.clf()
         print('plot created.')
+
     return model
 
 # calculate MAPE (timeseries evaluation metric)
@@ -73,7 +75,6 @@ def evaluate_cv(df_cv, polygon):
     for hr_range in hr_range_arr:
         # cut data
         df_cv = df_cv.iloc[0:hr_range*slices_per_hour]
-        df_cv = df_cv[['ds', 'y', 'yhat']]
 
         # define metrics & create score
         RMSE = mean_squared_error(df_cv.y, df_cv.yhat)
@@ -82,17 +83,32 @@ def evaluate_cv(df_cv, polygon):
         results.append({'POLYGON': polygon, 'HOUR': hr_range, 'RSQUARED': R2, 'RMSE': RMSE, 'MAPE': MAPE})
         # todo: uncertainty intervals/error bars?
 
-        # plot
-        if plotcrossvals:
-            df_plot = df_cv.set_index('ds')   # set timestamp as index
-            plt.figure
-            df_plot.plot()
-            plt.xlabel('timestamp')
-            plt.ylabel('attendees')
-            plt.title('hours considered: ' + str(hr_range))
-            plt.savefig('img/' + 'cv_' + filename_data + '_pol' + str(polygon) + '_' + str(hr_range) + 'h.png', bbox_inches='tight')
-            plt.close()
-            plt.clf()
+    # plot
+    df_plot = df_cv.set_index('ds')   # set timestamp as index
+    if plotcrossvals:
+        plt.figure
+        df_plot.plot()
+        plt.xlabel('timestamp')
+        plt.ylabel('attendees')
+        plt.title('Cross-Validation of Polygon ' + str(polygon))
+        plt.savefig('img/' + 'cv_' + filename_data + '_pol' + str(polygon) + '.png', bbox_inches='tight')
+        plt.close()
+        plt.clf()
+
+    if plotcvuncertainty:
+        fig = plt.figure(0)
+        plt.plot(df_plot.index, df_plot.y)
+        plt.plot(df_plot.index, df_plot.yhat)
+        #plt.errorbar(df_plot.index, df_plot.yhat, yerr=[y-df_plot.yhat_upper, y-df_plot.yhat_lower], uplims=True, lolims=True)
+        plt.fill_between(df_plot.index, df_plot.yhat_upper, df_plot.yhat_lower, facecolor='b', alpha=.3)
+        plt.legend(['y', 'y_hat', 'uncertainty'])
+        plt.xlabel('timestamp')
+        plt.ylabel('attendees')
+        plt.title('Cross-Validation of Polygon ' + str(polygon))
+        plt.savefig('img/' + 'cv_uc_' + filename_data + '_pol' + str(polygon) + '.png', bbox_inches='tight')
+        plt.show()
+        plt.close()
+        plt.clf()
     return results
 
 # parallelise calculations
@@ -101,7 +117,7 @@ import multiprocessing
 
 def runOneProcess(df_orig, polygon):
     model = forecast(df_orig, polygon)
-    crossvalidate(model, polygon)
+    if model: crossvalidate(model, polygon)
     return results
 
 def parallelise(df_orig, df_polygons):
@@ -114,11 +130,13 @@ def parallelise(df_orig, df_polygons):
 if __name__ == "__main__":
     # load data
     df_orig = pd.read_csv(basepath + filename_data)
-    df_polygons = pd.read_csv(basepath + filename_pols, nrows=10)   # todo: remove nrows
+    df_polygons = pd.read_csv(basepath + filename_pols, nrows=10)   # TODO:
     list_polygons = df_polygons.values.flatten()
     # parallelise processing
-    parallelise(df_orig, list_polygons)
+    #runOneProcess(df_orig, 13)
+    parallelise(df_orig, list_polygons) # TODO: change for debug
     # output data
+    print(results)
     df_results = pd.DataFrame(results[0][0])    # this list gets nested 2 times (why????) , therefore [0][0]
     df_results = df_results.set_index('POLYGON')
     df_results.to_csv('results.csv')
