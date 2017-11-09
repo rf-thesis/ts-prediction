4a6de87848266c5f6d933f9c45aa2c6863426357
l4u532 <k.langenheldt@gmail.com>
2017-11-09 08:54:32 +0100

    try sMAPE comparison

--- ts-compare-methods.py
+++ ts-compare-methods.py
@@ -219,10 +219,11 @@ def process(polygon):
 
 # run stuff
 #will not work for some polys
-#df_polygons = pd.read_csv('data/2017_polygoninfo_filtered.csv', usecols=["ogr_fid"], nrows=None)
-#polygon_list = df_polygons.values.astype(int).flatten()
-polygon_list = [6, 49, 18, 5, 25, 11, 16, 10]  # Inner Area, Orange, Rising, Camping C + E, Bridge, Tradezone, Street City
-autoARIMA = True
+df_polygons = pd.read_csv('data/2017_polygoninfo_filtered.csv', usecols=["ogr_fid"], nrows=None)
+polygon_list = df_polygons.values.astype(int).flatten()
+print(polygon_list)
+#polygon_list = [6, 49, 18, 5, 25, 11, 16, 10]  # Inner Area, Orange, Rising, Camping C + E, Bridge, Tradezone, Street City
+#autoARIMA = True
 
 if __name__ == '__main__':
     if isinstance(polygon_list, int):
